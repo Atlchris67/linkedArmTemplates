280076e94227d86847ff63da33bbe79b866423fa
Atlchris67 <atlchris@gmail.com>
2019-11-18 10:12:26 -0500

    added files]

---
+++ buildrg.ps1
@@ -0,0 +1,42 @@
+﻿$EnvName = 'helloword-dev' 
+$ResourceGroupName = $EnvName + '-rg'   
+#$KeyVaultName = $EnvName + "-kv"
+$Location = 'eastus'
+$Environment = 'dev'
+
+$TemplateFile = 'C:\devroot\improving\arm-templates\helloworldparent.json'
+#$TemplateParameterFile = 'C:\devroot\improving\cu-direct\rg-as-parts\keyvault\parameters.json'
+
+
+Get-AzContext
+#cat $TemplateParameterFile 
+#cat $TemplateFile
+
+try{
+    Get-AzResourceGroup -Name $ResourceGroupName -Location $Location -ErrorAction Stop
+    Get-AzResource | Where-Object{$_.ResourceGroupName -eq "$ResourceGroupName"} | Select-Object -Property Name
+}
+catch{
+    New-AzResourceGroup -Name $ResourceGroupName -Location $Location
+}
+
+
+
+#$ParametesFilesPath = "C:\devroot\improving\cu-direct\rg-as-parts\"
+
+#$paramFiles = Get-ChildItem -Path $ParameteFilePath -Force -Recurse -Include *.parameters.json
+
+#$paramFiles | ForEach-Object -Process {
+
+    $TemplateParams = @{
+        ResourceGroupName = $ResourceGroupName  
+        TemplateFile = $TemplateFile
+        #TemplateFile = $_.Directory.FullName + "\" + $_.Name.Substring( 0,$_.Name.IndexOf('.') ) + ".json"
+        #TemplateParameterFile = $_.FullName
+    }
+
+    New-AzResourceGroupDeployment @TemplateParams
+
+#}
+
+
